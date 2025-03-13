40e1e1e5522df2ef28439ef8fe7e32fd7bd78740
Arthur Canton <163457540+Tubuia@users.noreply.github.com>
2025-03-12 21:23:15 -0300

    Add files via upload

---
+++ prog-udf/aula3/exercicio5.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite o seu nome: '))
+idade = int(input('Digite a sua idade: '))
+
+print(f'Ola {nome}! Voce tem {idade} anos?')
+print(f'Você nasceu em {2025 - idade}')
+print(nome * idade)
