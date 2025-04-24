dc0747180b70d0e846c95ae9f9328ecfc426688a
Arthur Canton <163457540+Tubuia@users.noreply.github.com>
2025-04-23 21:43:02 -0300

    tarefa 2 aula 9
    tarefa 2 da aula 9

---
+++ aula9/exercicio002.py
@@ -0,0 +1,16 @@
+#Imprima o total de episódios
+#Substitua a quantidade de episódios pelo percentual (ep_anime/ep_total)
+#Imprima nome e percentual de cada anime, em ordem decrescente de percentual
+
+animes = {
+    "Naruto": 220,
+    "Jujutsu Kaisen": 47,
+    "Dragon Ball Z": 291,
+    "Death Note": 37,
+    "Fullmetal Alchemist": 64,
+    "Evangelion": 26,
+    "Berserk": 25,
+    "Code Geass": 50,
+    "Akame ga Kill!": 24,
+    "Elfen Lied": 13
+}
